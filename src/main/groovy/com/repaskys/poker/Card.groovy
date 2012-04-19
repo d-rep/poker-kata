88069d65bd4344fabf54b986051aaf7b56bac9d5
Drew Repasky <drew.repasky@gmail.com>
2012-04-18 20:11:35 -0400

    minor corrections to the comments

--- src/main/groovy/com/repaskys/poker/Card.groovy
+++ src/main/groovy/com/repaskys/poker/Card.groovy
@@ -6,13 +6,13 @@ import com.repaskys.poker.Suit
 class Card {
 
    /**
-    * This is the face value of the card. 1-9, T for a "Ten", J for "Jack", Q for "Queen", K for "King", and A for "Ace."
+    * This is the face value of the card. 2-9, T for a "Ten", J for "Jack", Q for "Queen", K for "King", and A for "Ace."
     */
    String value
    Suit suit
 
    /**
-    * This accepts a 2-character abbreviation of the card, such as "2C" for the Two of Clubs.
+    * This accepts a 2-character abbreviation of the card with the face value and then the suit, such as "2C" for the Two of Clubs.
     */
    public static Card create(String abbr) {
       return new Card(value: abbr[0], suit: abbr[1])
