6c0d800d75019016c5fe60bf2f0c14a3d28aae44
Drew Repasky <drew.repasky@gmail.com>
2012-04-21 22:24:58 -0400

    Added method to group cards with the same face value, to check for pairs

--- src/main/groovy/com/repaskys/poker/Hand.groovy
+++ src/main/groovy/com/repaskys/poker/Hand.groovy
@@ -1,10 +1,33 @@
 package com.repaskys.poker
 
+import static com.repaskys.poker.Pairs.*
+
 @Immutable
 class Hand extends ArrayList<Card> {
+
    boolean isFlush() {
       // Get the "suit" property of every item, put it in a Set to ensure we only
       // keep uniques, then make sure that collection contains only one item.
       return (this*.suit as Set).size() == 1
+   }
+
+   /**
+    * Group any cards that have the same face value, which is used to determine pairs.
+    * Entries in the map have a key with the face value of the card and a value containing the amount of matches.
+    */
+   Map<String, Integer> groupLikeCards() {
+
+      def likeCards = [:]
+
+      this.each{ card ->
+         def value = card.value
+         def matches = likeCards[value]
+         if(matches == null) {
+            matches = 0
+         }
+         likeCards[value] = matches + 1
+      }
+
+      return likeCards
    }
 }
--- src/test/groovy/com/repaskys/poker/HandTest.groovy
+++ src/test/groovy/com/repaskys/poker/HandTest.groovy
@@ -3,10 +3,13 @@ package com.repaskys.poker
 import groovy.util.GroovyTestCase
 
 import static com.repaskys.poker.Card.DECK as D
+import static com.repaskys.poker.Pairs.*
+
 import com.repaskys.poker.Hand
 import com.repaskys.poker.Suit
 
 class HandTest extends GroovyTestCase {
+
    void testFlush() {
       def hand = [D."2C", D."3C", D."4C", D."5C", D."8C"] as Hand
       assert hand.isFlush()
@@ -16,4 +19,30 @@ class HandTest extends GroovyTestCase {
       def hand = [D."2C", D."3C", D."4C", D."5C", D."8D"] as Hand
       assert ! hand.isFlush()
    }
+
+   void testGroupLikeCardsNoPairs() {
+      def hand = [D."2C", D."3C", D."4C", D."5C", D."8D"] as Hand
+      assert ["2":1, "3":1, "4":1, "5":1, "8":1] == hand.groupLikeCards()
+   }
+
+   void testGroupLikeCardsFourOfAKind() {
+      def hand = [D."2C", D."2S", D."2D", D."2H", D."8D"] as Hand
+      assert ["2":4, "8":1] == hand.groupLikeCards()
+   }
+
+   void testGroupLikeCardsThreeOfAKind() {
+      def hand = [D."2C", D."2S", D."2D", D."3H", D."8D"] as Hand
+      assert ["2":3, "3":1, "8":1] == hand.groupLikeCards()
+   }
+
+   void testGroupLikeCardsTwoPairs() {
+      def hand = [D."2C", D."2S", D."3D", D."3H", D."8D"] as Hand
+      assert ["2":2, "3":2, "8":1] == hand.groupLikeCards()
+   }
+
+   void testGroupLikeCardsOnePair() {
+      def hand = [D."2C", D."2S", D."3D", D."4H", D."8D"] as Hand
+      assert ["2":2, "3":1, "4":1, "8":1] == hand.groupLikeCards()
+   }
+
 }
